42484a2ad2318d84788c728e695f4f3b2be6cba9
Joel Santos <joeljesussantos50@gmail.com>
2022-06-06 23:39:47 +0200

    Uses kivy instead Tkinter to do interface

--- interface.py
+++ interface.py
@@ -1,41 +1,31 @@
 #!/usr/bin/env python3
 
-import tkinter as tk
-import download as dl
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty
+
+kivy.require("2.1.0")
+
+class LabelInput(BoxLayout):
+    label_text = StringProperty("default")
+    input_text = StringProperty("")
+
+    def alter_input_text(self):
+        self.input_text = self.ids.text_input.text
 
 
-class PEntry():
-    def __init__(self, master, text):
-        self.root = tk.Frame(master)
-        self.label = tk.Label(self.root, text=text, width=9, anchor='w')
-        self.entry = tk.Entry(self.root)
-    
-    def grid(self, **kwargs):
-        self.root.grid(**kwargs, padx=10, pady=(10, 0))
-        self.label.grid(row=0, column=0)
-        self.entry.grid(row=0, column=1)
-    
-    def get_value(self):
-        return self.entry.get()
-  
+class Interface(BoxLayout):
+    pass
 
-def main():
-    root = tk.Tk()
-    root.resizable(False, False)
-    # root_tk.geometry("400x240")
-    
-    entry1 = PEntry(root, "username:")
-    entry2 = PEntry(root, "playlist:")
 
-    entry1.grid(row=0, column=0)
-    entry2.grid(row=1, column=0)
+class MusicDownloaderApp(App):
 
-    down = lambda: dl.download(entry1.get_value(), entry2.get_value())
-    button = tk.Button(master=root, text="Download", command=down)
-    button.grid(row=3, column=0, sticky=tk.N, pady=10)
-
-    root.mainloop()
+    def build(self):
+        return Interface()
 
 
 if __name__ == '__main__':
-    main()
+    MusicDownloaderApp().run()
