5643652cb525fb6bfdd8940136ad2da980188c69
Joel Santos <joeljesussantos50@gmail.com>
2023-05-25 19:46:15 +0100

    Rewrites the UI. Progress bar not working yet

--- interface.py
+++ interface.py
@@ -1,61 +1,50 @@
 #!/usr/bin/env python3
 
-import kivy
+import threading
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.config import Config
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 import download as dl
 
-kivy.require("2.1.0")
 
-class LabelInput(BoxLayout):
-    label_text = StringProperty("default")
-    input_text = StringProperty("")
+class Error(Popup):
+    text = StringProperty("")
 
-    def alter_input_text(self):
-        self.input_text = self.ids.text_input.text
-
-
-class Interface(BoxLayout):
-    pass
+    def __init__(self, text, **kwargs):
+        super().__init__(**kwargs)
+        self.text = text
 
 
-class PopupWidget(BoxLayout):
-    
-    def get_error_text(self):
-        with open('error.txt', 'r') as f:
-            return f.read()
-
-
-class ErrorPopup(Popup):
-    pass
-
-
-class DownloadButton(Button):
-
-    def throw_popup(self):
-        popup = ErrorPopup()
-        popup.open()
-
-
+class MainLayout(BoxLayout):
     def download(self, username, playlist):
-        has_error = dl.download(username, playlist)
-        if type(has_error) == str:
-            self.throw_popup()
+        if threading.active_count() <= 2:
+            error = dl.download(username, playlist)
+
+            if type(error) == str:
+                Clock.schedule_once(lambda dt: self.throw_popup(error), -1)
+
+    def thread_download(self, username, playlist):
+        threading.Thread(target=self.download,
+            args=(username, playlist),
+            # daemon=True
+            ).start()
+    
+    def throw_popup(self, error):
+            popup = Error(text=error)
+            popup.open()
 
 
 class MusicDownloaderApp(App):
-
-    def build(self):
-        Config.set('graphics', 'width', '500')
-        Config.set('graphics', 'height', '150')
-        Config.set('graphics', 'resizable', False)
-
-        return Interface()
+    pass
 
 
 if __name__ == '__main__':
+    Config.set('graphics', 'width', '500')
+    Config.set('graphics', 'height', '170')
+    Config.set('graphics', 'resizable', False)
+    # Config.set('kivy', 'log_level', 'error')
+    # Config.set('kivy', 'log_enable', 0)
     MusicDownloaderApp().run()
